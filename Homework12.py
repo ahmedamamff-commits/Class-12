2b5f233d2504b926332e50678558bac1836dd8d3
ahmedamamff-commits <ahmedamamff@gmail.com>
2026-08-29 14:20:11 -0700

    Add files via upload

---
+++ Homework12.py
@@ -0,0 +1,13 @@
+import turtle #importing library
+turtle.Screen().bgcolor("orange")
+Pen = turtle.Turtle()
+
+Pen.forward(90)
+Pen.right(90)
+Pen.forward(90)
+Pen.right(90)
+Pen.forward(90)
+Pen.right(90)
+Pen.forward(90)
+
+
